41f9e2f6b239c2c3ea8f3aa4551a4a1d2fc76b6c
stephy-VS <vsstephy.ptf@gmail.com>
2024-06-14 17:28:24 +0530

    awsKey-evn1

--- MuseumTicketing/src/main/java/com/example/MuseumTicketing/Config/SecurityConfig.java
+++ MuseumTicketing/src/main/java/com/example/MuseumTicketing/Config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
