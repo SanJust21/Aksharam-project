955a89d2b17ffd60527d5b5e79bc1d5db6400377
stephy-vs <vsstephy.ptf@gmail.com>
2024-10-25 11:16:37 +0530

    localServerDetails-getstream 10kb.25-October2024

--- MuseumTicketing/src/main/java/com/example/MuseumTicketing/localServer/Digital/mainHeading/MainTitleService.java
+++ MuseumTicketing/src/main/java/com/example/MuseumTicketing/localServer/Digital/mainHeading/MainTitleService.java
@@ -708,7 +708,7 @@ public class MainTitleService {
             StreamingResponseBody responseBody = outputStream -> {
                 try (InputStream inputStream = resource.getInputStream()) {
                     inputStream.skip(finalStart);
-                    byte[] buffer = new byte[8192]; //8kb
+                    byte[] buffer = new byte[10240]; //8kb
                     long bytesRead;
                     long totalBytesRead = 0;
 
