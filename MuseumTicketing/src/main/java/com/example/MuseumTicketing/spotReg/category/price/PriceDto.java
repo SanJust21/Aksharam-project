8d031d57a630041bb91a6e0b2bc6adc335df95f6
stephy-vs <vsstephy.ptf@gmail.com>
2024-10-29 21:31:21 +0530

    getPriceResponse-category_N_price_Name-29-October2024-v2.1

---
+++ MuseumTicketing/src/main/java/com/example/MuseumTicketing/spotReg/category/price/PriceDto.java
@@ -0,0 +1,10 @@
+package com.example.MuseumTicketing.spotReg.category.price;
+
+import lombok.Data;
+
+@Data
+public class PriceDto {
+    private String categoryName;
+    private String typeName;
+    private Double price;
+}
